e79d27596f0c933775eba1f3079b801cba84e89c
Sidney <sid@onlysid.com>
2024-04-26 16:58:06 +0100

    Filter search cleanup

--- views/dashboard.php
+++ views/dashboard.php
@@ -388,11 +388,11 @@ foreach($resultItems as $result) : ?>
                     <label for="filterSearch">Search results</label>
                     <div class="input-wrapper !py-1 !pr-1">
                         <input id="filterSearch" type="text" placeholder="Search..." name="s" value="<?= $_GET['s'] ?? "";?>">
-                        <button class="aspect-square rounded-full p-2 bg-dark transition-all duration-500 hover:scale-110 hover:opacity-75" type="submit">
+                        <div class="aspect-square rounded-full p-2 bg-dark transition-all duration-500 hover:scale-110 hover:opacity-75">
                             <svg class="h-3 w-auto fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                             </svg>
-                        </button>
+                        </div>
                     </div>
                 </div>
             </div>
